a52255a71f0e6d5a8cdae533e5c4e32fcd8f12d9
szpak2100 <szpak2100@gmail.com>
2021-05-23 08:59:06 +0200

    Chapter 1 finished

---
+++ LearnCpp4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout<<"Enter an integer: ";
+    cin>>x;
+    cout<<"Enter another  integer: ";
+    cin>>y;
+
+    cout<< x <<"+"<< y <<" is "<< x+y<<endl;
+    cout<< x <<"-"<< y <<" is "<< x-y<<endl;
+
+    return 0;
+}
